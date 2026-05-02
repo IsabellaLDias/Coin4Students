5acdff1a4892a5ae0176077d27cd88cc44604226
Isabella Dias <isabellamg2017@gmail.com>
2026-05-02 17:18:21 -0300

    [FIX] Habilitação CORS

--- back/aluno-service/src/main/java/com/coin4students/aluno/controller/AlunoController.java
+++ back/aluno-service/src/main/java/com/coin4students/aluno/controller/AlunoController.java
@@ -1,11 +1,13 @@
 package com.coin4students.aluno.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import com.coin4students.aluno.model.Aluno;
 import com.coin4students.aluno.service.AlunoService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/alunos")
 @CrossOrigin(origins = "*")
--- back/empresa-service/src/main/java/com/coin4students/empresa/controller/EmpresaController.java
+++ back/empresa-service/src/main/java/com/coin4students/empresa/controller/EmpresaController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.coin4students.empresa.model.Empresa;
 import com.coin4students.empresa.service.EmpresaService;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/empresas")
 @CrossOrigin(origins = "*")
