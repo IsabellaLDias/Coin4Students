ef90752a81d173abd6ce604661c2196dc0a1e44b
Isabella Dias <isabellamg2017@gmail.com>
2026-05-19 19:50:57 -0300

    [BACK] Sistema Antisono Render

---
+++ back/transacao-service/src/main/java/com/coin4students/transacao/controller/PingController.java
@@ -0,0 +1,24 @@
+package com.coin4students.transacao.controller;
+
+import br.com.vaicomigo.infra.security.UsuarioAutenticado;
+import br.com.vaicomigo.usuarios.api.UsuarioResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/ping")
+@RequiredArgsConstructor
+public class PingController {
+
+    @GetMapping
+    public ResponseEntity<String> ping() {
+        log.info("Ping recebido no backend!");
+        return ResponseEntity.ok("Acordado!");
+    }
+}
